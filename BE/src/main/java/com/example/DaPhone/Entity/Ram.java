bd35484a134643da4c90b83f1575153dd34fbe64
dangngocphuc <dangngocphuc19@gmail.com>
2023-02-27 14:07:17 +0700

    cm code 27/02/2023

--- BE/src/main/java/com/example/DaPhone/Entity/Ram.java
+++ BE/src/main/java/com/example/DaPhone/Entity/Ram.java
@@ -12,13 +12,11 @@ import java.io.Serializable;
 @Setter
 public class Ram implements Serializable {
 
+	@Id
+	@SequenceGenerator(name = "seqRam", sequenceName = "SEQ_Ram", allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqRam")
+	private Long id;
 
-
-        @Id
-        @SequenceGenerator(name = "seqRam", sequenceName = "SEQ_Ram", allocationSize = 1, initialValue = 1)
-        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqRam")
-        private Long id;
-
-        @Column(name="DungLuongRam")
-        private String dungluongram;
+	@Column(name = "ram")
+	private String ram;
 }
